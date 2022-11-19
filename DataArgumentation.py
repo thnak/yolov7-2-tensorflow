05a387dec27691c5fe1634b9b891720db84155e0
thnak <thanhhero765@gmail.com>
2022-11-19 23:57:03 +0700

    modified:   DataArgumentation.py 	new file:   inference/bird-4887736.txt

--- DataArgumentation.py
+++ DataArgumentation.py
@@ -30,7 +30,7 @@ def visualize_bbox(img, bbox, class_name, color=BOX_COLOR, thickness=2):
     return img
 
 
-def visualize(image, bboxes, category_ids, category_id_to_name, filename=''):
+def visualize(image, bboxes, category_ids, category_id_to_name):
     img = image.copy()
     for bbox, category_id in zip(bboxes, category_ids):
         class_name = category_id_to_name[category_id]
@@ -38,38 +38,81 @@ def visualize(image, bboxes, category_ids, category_id_to_name, filename=''):
     img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     return img
     
-def DataArgumentation(imginputPath='', bboxInputPath='', bbboxOutputPath='', imgOutputPath='', cropSize=[416,416], showImg=False):
+def getName_extention(stringPath=''):
+    file_ = os.path.basename(stringPath)
+    fileName = os.path.splitext(file_)
+    return fileName
     
+    
+def readYoLoBbox(stringPath=''):
+    txt = open(stringPath, 'r')
+    stringtxt = txt.read()
+    arr = stringtxt.split('\n')
+    aux= []
+    for i in arr:
+        a = i.split(' ')
+        au = list(map(float,a))
+        au[0] = int(au[0])
+        aux.append(au)
+    return aux
+    
+def DataArgumentation(imginputPath='', bboxInputPath='', bbboxOutputPath='', imgOutputPath='', cropSize=[416,416], showImg=False):
+    if bboxInputPath =='':
+        bboxInputPath = imginputPath
+        print('Bbox input path is null, using img input path to scan...\n')
+    
+    print(f'Scaning {imginputPath}...')
     arrImageInputPath_, arrImageInputPath=[], []
     arrImageInputPath_ = os.listdir(imginputPath)
-    
+    print(arrImageInputPath_)
     cou = 0
     for imgpath in arrImageInputPath_:
-        if os.path.isfile(imgpath):
-            if imgpath.endswith('.jpg') or imgpath.endswith('.png'):
-                arrImageInputPath.append(imgpath)
-                cou+=1
-    print(f'Total {cou} image files\n')
+        if imgpath.endswith('.jpg') or imgpath.endswith('.png') or imgpath.endswith('.jpeg') or imgpath.endswith('.PNG') or imgpath.endswith('.JPG'):
+            arrImageInputPath.append(imgpath)
+            cou+=1
+    print(f'Found total {cou} image files')
     
-    arrBboxOutputPath, arrBboxOutputPath_ = [], []
-    arrBboxOutputPath_ = os.listdir(bboxInputPath)
+    arrBboxInputPath, arrBboxInputPath_ = [], []
+    arrBboxInputPath_ = os.listdir(bboxInputPath)
     cou = 0
-    for imgpath in arrBboxOutputPath_:
+    for imgpath in arrBboxInputPath_:
         if os.path.isfile(imgpath) and imgpath.endswith('.txt'):
-            arrBboxOutputPath.append(imgpath)
+            arrBboxInputPath.append(imgpath)
             cou +=1
-    print(f'Total {cou} txt files\n')
+    print(f'Found total {cou} txt files')
     
+    img_bbox = []
     for imgpath in arrImageInputPath:
-        pass
+        imgNamed = getName_extention(imgpath)[0]
+        for arrBbox in arrBboxInputPath:
+            namedBbox = getName_extention(arrBbox)[0]
+            if imgNamed in namedBbox or imgNamed == namedBbox:
+                ar = [imgpath, arrBbox]
+                img_bbox.append(ar)
+            else:
+                print(f'{imgpath} were not labeled')
+    
+    print(f'Total {len(img_bbox)} files was labeled, processing...')
     
     
-    image = cv2.imread('inference/bird-4887736.png')
+    for img_bbox_ in img_bbox:
+        image_, bbox_= img_bbox_[0], img_bbox_[1]
+        bbox = readYoLoBbox(bbox_)
+        image_ = imginputPath+'/'+image_
+        bbox_ = bboxInputPath+'/'+bbox_
+        process(imgPath=image_, bboxes=bbox, viewImg=True)
+        
+def process(imgPath='', bboxes=[], category_ids=[], category_id_to_name={0: 'pig',1:'dog'}, imgIndex=0, viewImg=False,imgOutputPath='', bboxOutputPath=''):
+    image = cv2.imread(imgPath)
+    print(imgPath)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    category_ids = category_ids
+    for _ in range(len(bboxes)):
+        category_ids.append(bboxes[_][0])
+        bboxes[_] = [bboxes[_][1], bboxes[_][2], bboxes[_][3],bboxes[_][4]]
     
-    bboxes = [[0.704009, 0.580384, 0.466981,0.839232],[0.307193, 0.621161, 0.515330, 0.757678]]
-    category_ids = [17,18]
-    category_id_to_name = {17: 'cat',18:'dog'}
+    
+    # bboxes = [[0.704009, 0.580384, 0.466981,0.839232],[0.307193, 0.621161, 0.515330, 0.757678]]
 
     p = 1/4
     transform = A.Compose(
@@ -88,13 +131,20 @@ def DataArgumentation(imginputPath='', bboxInputPath='', bbboxOutputPath='', img
         bbox_params=A.BboxParams(format='yolo', label_fields=['category_ids']),
     )
     count = 0
+    print(bboxes)
+    print(category_ids)
     for _ in range(40):
-        name = 'deploy/img_s' +str(count)+'.jpg'
+        nameImg = imgOutputPath+'index' +str(imgIndex)+str(count)+'.jpg'
         count+=1
         transformed = transform(image=image, bboxes=bboxes, category_ids=category_ids)
-        img = visualize(transformed['image'],transformed['bboxes'], transformed['category_ids'],category_id_to_name,name)
-        cv2.namedWindow('a',cv2.WINDOW_NORMAL)
-        cv2.imwrite(name,img,[cv2.IMWRITE_JPEG_QUALITY, 75])
-        cv2.imshow('a',img)
+        img = visualize(transformed['image'],transformed['bboxes'], transformed['category_ids'],category_id_to_name)
+        print(transformed['bboxes'])
+        # cv2.imwrite(nameImg,img,[cv2.IMWRITE_JPEG_QUALITY, 75])
+        if viewImg:
+            cv2.namedWindow('a',cv2.WINDOW_NORMAL)
+            cv2.imshow('a',img)
         if cv2.waitKey(1) == 27:
             break
+
+
+DataArgumentation(imginputPath='inference')
