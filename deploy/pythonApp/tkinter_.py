b15cda76c01204d123b91144301f2b218736ccb2
thnak <thanhhero765@gmail.com>
2022-11-19 12:25:48 +0700

    new file:   DataArgumentation.py 	deleted:    album.py 	new file:   deploy/pythonApp/__pycache__/tkinter.cpython-310.pyc 	new file:   deploy/pythonApp/tkinter_.py 	renamed:    app/bird-4887736.png -> inference/bird-4887736.png 	deleted:    inference/img_0.jpg 	deleted:    inference/img_1.jpg 	deleted:    inference/img_10.jpg 	deleted:    inference/img_11.jpg 	deleted:    inference/img_12.jpg 	deleted:    inference/img_13.jpg 	deleted:    inference/img_2.jpg 	deleted:    inference/img_3.jpg 	deleted:    inference/img_4.jpg 	deleted:    inference/img_5.jpg 	deleted:    inference/img_6.jpg 	deleted:    inference/img_7.jpg 	deleted:    inference/img_8.jpg 	deleted:    inference/img_9.jpg

---
+++ deploy/pythonApp/tkinter_.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+from tkinter import messagebox
+ 
+top = Tk()
+top.geometry("200x100")
+ 
+def clickRedButton():
+    messagebox.showinfo("Hello", "Red Button clicked")
+ 
+def clickBlueButton():
+    messagebox.showinfo("Hello", "Blue Button clicked")
+ 
+b1 = Button(top, text = "Red", command = clickRedButton, activeforeground = "red", activebackground = "pink", pady = 10)
+b2 = Button(top, text = "Blue", command = clickBlueButton, activeforeground = "blue", activebackground = "pink", pady = 10)
+b3 = Button(top, text = "Green", activeforeground = "green", activebackground = "pink", pady = 10)
+b4 = Button(top, text = "Yellow", activeforeground = "yellow", activebackground = "pink", pady = 10)
+b1.pack(side = LEFT)
+b2.pack(side = RIGHT)
+b3.pack(side = TOP)
+b4.pack(side = BOTTOM)
+top.mainloop()
