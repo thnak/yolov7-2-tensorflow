9189533cb286f5ff5a789585837a8b0808bfbbdd
thnak <117495750+thnak@users.noreply.github.com>
2023-04-20 16:13:11 +0700

    .

--- models/experimental.py
+++ models/experimental.py
@@ -248,7 +248,9 @@ def attempt_load(weights, map_location=None):
     for w in weights if isinstance(weights, list) else [weights]:
         attempt_download(w)
         ckpt = torch.load(w, map_location=map_location)  # load
-        model.append(ckpt['ema' if 'ema' in ckpt else 'model'].float().fuse().eval())  # FP32 model
+        m = ckpt.get("ema", ckpt.get('model'))
+        m = ckpt.get("model") if not m else m
+        model.append(m.float().fuse().eval())  # FP32 model
 
     # Compatibility updates
     for m in model.modules():
