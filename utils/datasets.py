891ede10348bb5e11dc51ec7b31d7b4fe16e8870
Văn Thạnh <thanhhero765@gmail.com>
2023-11-21 23:10:13 +0700

    remove print

--- utils/datasets.py
+++ utils/datasets.py
@@ -1562,7 +1562,6 @@ def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scale
         img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
     top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
     left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
-    print(f"{left} {top} {bottom} {right}")
     img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
     return img, ratio, (dw, dh)
 
