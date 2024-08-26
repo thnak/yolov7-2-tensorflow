8220595140dd0e979f1ec50a830e675fc83709f6
Văn Thạnh <117495750+thnak@users.noreply.github.com>
2024-08-26 11:45:47 +0700

    Update datasets.py
    Signed-off-by: Văn Thạnh <117495750+thnak@users.noreply.github.com>

--- utils/datasets.py
+++ utils/datasets.py
@@ -944,7 +944,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
         self.label_files = img2label_paths(self.img_files)  # labels
         cache_path = (p if p.is_file() else Path(self.label_files[0]).parent).with_suffix('.cache')  # cached labels
         if cache_path.is_file():
-            cache, exists = torch.load(cache_path), True  # load
+            cache, exists = torch.load(cache_path, weights_only=False), True  # load
             if cache['hash'] != get_hash(self.label_files + self.img_files) or cache[
                 'version'] != self.version:  # changed
                 cache, exists = self.cache_labels(cache_path, prefix), False  # re-cache
